c3fd538d5175eebdd9aa4cc3daee50615ce6ddb0
Ваше Имя <you@example.com>
2019-04-14 01:03:26 +0400

    Hello world

--- public/script.js
+++ public/script.js
@@ -1,3 +1,13 @@
+function func(param){
+	$(document).ready(()=>{
+		$('.pup').css('display','block')
+		$('.window>center>iframe').attr('src',param.children[2].innerHTML)
+		$('.window>center>iframe').css('height','400px')
+	})
+}
+window.ondblclick = ()=>{
+	$('.pup').css('display','none')
+}
 window.onload = ()=>{
 	// Initial state
 	var scrollPos = 0
@@ -12,11 +22,136 @@ window.onload = ()=>{
 			$('#menu>ul>li').css('font-size',$('#menu').width()*1.5/100 + 'px')
 		},100)
 		$('#home').click(()=>{
-			window.scrollTo(0,$('header').height() + 10)
+			var k = 0;
+			if(window.scrollY > $('header').height() + 100)
+			{
+				var inter = setInterval(()=>{
+					if(window.scrollY < $('header').height() + 100)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + 10)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY - k)
+					}
+				},10)
+			}
 		})
 		$('#kyanq').click(()=>{
-			window.scrollTo(0,$('header').height() + 10)
+			var k = 0;
+			if(window.scrollY > $('header').height() + 100)
+			{
+				k = 0
+				var inter = setInterval(()=>{
+					if(window.scrollY < $('header').height() + 100)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + 10)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY - k)
+					}
+				},10)
+			}
 		})
+		$('#erger').click(()=>{
+			var k = 0;
+			if(window.scrollY > $('header').height() + $('.k').height() + 100)
+			{
+				k = 0
+				var inter = setInterval(()=>{
+					if(window.scrollY < $('header').height() + $('.k').height() + 100)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + $('.k').height() + 100)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY - k)
+					}
+				},10)
+			}
+			else{
+				k = 0
+				var inter = setInterval(()=>{
+					if(window.scrollY > $('header').height() + $('.k').height() + 100)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + $('.k').height() + 100)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY + k)
+					}
+				},10)
+			}
+		})
+		$('#mtqer').click(()=>{
+			var k = 0;
+			if(window.scrollY > $('header').height() + $('.k').height() + $('.e').height() + 160)
+			{
+				k = 0
+				var inter = setInterval(()=>{
+					if(window.scrollY < $('header').height() + $('.k').height() + $('.e').height() + 160)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + $('.k').height() + $('.e').height() + 160)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY - k)
+					}
+				},10)
+			}
+			else{
+				k = 0
+				var inter = setInterval(()=>{
+					if(window.scrollY > $('header').height() + $('.k').height() + $('.e').height() + 160)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + $('.k').height() + $('.e').height() + 160)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY + k)
+					}
+				},10)
+			}
+		})
+		/*$('#nkarner').click(()=>{
+			var k = 0;
+			if(window.scrollY > $('header').height() + $('.k').height() + $('.e').height() + $('.m').height() + 240)
+			{
+				k = 0
+				var inter = setInterval(()=>{
+					if(window.scrollY < $('header').height() + $('.k').height() + $('.e').height() + $('.m').height() + 240)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + $('.k').height() + $('.e').height() + $('.m').height() + 240)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY - k)
+					}
+				},10)
+			}
+			else{
+				k = 0
+				var inter = setInterval(()=>{
+					if(window.scrollY > $('header').height() + $('.k').height() + $('.e').height() + $('.m').height() + 240)
+					{
+						clearInterval(inter)
+						window.scrollTo(0,$('header').height() + $('.k').height() + $('.e').height() + $('.m').height() + 240)
+					}
+					else{
+						k++
+						window.scrollTo(0,window.scrollY + k)
+					}
+				},10)
+			}
+		})*/
 		window.addEventListener('scroll', ()=>{
 			var scroll = document.documentElement.scrollTop
 			if(scroll >= $('header').height())
